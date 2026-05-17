bb674debee9354140545ebfcf79cadb5614e3297
Anseltsmm <anseltsm24@gmail.com>
2026-05-17 10:29:59 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,10 +33,10 @@ API_KEY = os.getenv("BINANCE_API_KEY")
 API_SECRET = os.getenv("BINANCE_API_SECRET")
 
 SYMBOL = os.getenv("SYMBOL", "DOGEUSDT")
-INTERVAL = os.getenv("INTERVAL", "3m")
+INTERVAL = os.getenv("INTERVAL", "1m")
 
-LEVERAGE = int(os.getenv("LEVERAGE", 5))
-ORDER_USDT = float(os.getenv("ORDER_USDT", 20))
+LEVERAGE = int(os.getenv("LEVERAGE", 10))
+ORDER_USDT = float(os.getenv("ORDER_USDT", 1))
 
 TP_ROI = float(os.getenv("TP_ROI", 0.004))
 SL_ROI = float(os.getenv("SL_ROI", 0.003))
