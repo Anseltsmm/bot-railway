8bdebc1fe841a9439c396503552e337eb0b95e77
Anseltsmm <anseltsm24@gmail.com>
2026-05-18 15:40:15 +0700

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -2,5 +2,7 @@ from flask_socketio import SocketIO
 
 socketio = SocketIO(
     cors_allowed_origins="*",
-    async_mode="eventlet"
+    async_mode="eventlet",
+    ping_timeout=60,
+    ping_interval=25
 )
