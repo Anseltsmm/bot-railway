91e8b0b34a81b6f25dfe41404cd1dad773da6869
Anseltsmm <anseltsm24@gmail.com>
2026-05-18 20:00:06 +0700

    Update state.py

--- state.py
+++ state.py
@@ -1,50 +1,70 @@
 bot_state = {
 
+    # =========================
+    # MARKET
+    # =========================
+
     "symbol": "DOGEUSDT",
-
-    "signal": "NONE",
-
     "price": 0,
-
     "balance": 0,
 
-    "position": "NONE",
+    # =========================
+    # POSITION
+    # =========================
 
+    "position": "NONE",
     "entry": 0,
 
     "sl_price": 0,
-
     "tp_price": 0,
-
     "trail": 0,
 
-    "pnl": 0,
+    # =========================
+    # PNL
+    # =========================
 
+    "pnl": 0,
     "pnl_idr": 0,
 
-    "rsi": 0,
+    # =========================
+    # SIGNAL
+    # =========================
+
+    "signal": "NONE",
 
     "trend": "-",
-
     "structure": "-",
-
-    "long_score": 0,
-
-    "short_score": 0,
 
     "confidence": 0,
 
-    "mtf_bullish": 0,
+    # =========================
+    # SCORE
+    # =========================
 
-    "mtf_bearish": 0,
+    "long_score": 0,
+    "short_score": 0,
+
+    "bullish_tf": 0,
+    "bearish_tf": 0,
+
+    "rsi": 0,
+
+    # =========================
+    # TRADE STATS
+    # =========================
 
     "trade_count": 0,
+    "win_trade": 0,
+    "lose_trade": 0,
 
     "winrate": 0,
 
+    "last_result": "-",
+
+    # =========================
+    # INTERNAL
+    # =========================
+
     "last_trade_time": 0,
-
-    "mtf": {},
-
-    "screener": []
+    "last_position_amt": 0
 }
