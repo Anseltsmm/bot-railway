dc4d96ac56e8ded98e776a772db21687f44dcc8c
Anseltsmm <anseltsm24@gmail.com>
2026-05-18 17:05:08 +0700

    Create state.py

---
+++ state.py
@@ -0,0 +1,48 @@
+bot_state = {
+
+    "symbol": "DOGEUSDT",
+
+    "signal": "NONE",
+
+    "price": 0,
+
+    "balance": 0,
+
+    "position": "NONE",
+
+    "entry": 0,
+
+    "sl_price": 0,
+
+    "tp_price": 0,
+
+    "trail": 0,
+
+    "pnl": 0,
+
+    "pnl_idr": 0,
+
+    "rsi": 0,
+
+    "trend": "-",
+
+    "structure": "-",
+
+    "long_score": 0,
+
+    "short_score": 0,
+
+    "confidence": 0,
+
+    "mtf_bullish": 0,
+
+    "mtf_bearish": 0,
+
+    "trade_count": 0,
+
+    "winrate": 0,
+
+    "mtf": {},
+
+    "screener": []
+}
