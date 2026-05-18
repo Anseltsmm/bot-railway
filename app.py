33e826cd43b0ad087c4387a8ad328bef382ccc0b
Anseltsmm <anseltsm24@gmail.com>
2026-05-18 10:19:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,16 +8,13 @@ eventlet.monkey_patch()
 from flask import Flask, render_template, jsonify
 
 from extensions import socketio
-from bot import run_bot, web_data
+from bot import start_bot, web_data
 
 # =========================
-# CREATE FLASK APP
+# FLASK
 # =========================
 app = Flask(__name__)
 
-# =========================
-# INIT SOCKETIO
-# =========================
 socketio.init_app(
     app,
     cors_allowed_origins="*"
@@ -28,89 +25,21 @@ socketio.init_app(
 # =========================
 @app.route("/")
 def home():
-
-    return render_template(
-        "dashboard.html"
-    )
+    return render_template("dashboard.html")
 
 @app.route("/api/data")
 def api_data():
-
-    return jsonify(
-        web_data
-    )
+    return jsonify(web_data)
 
 # =========================
-# HEALTH CHECK
-# =========================
-@app.route("/health")
-def health():
-
-    return {
-
-        "status": "running",
-
-        "bot": "active"
-    }
-
-# =========================
-# REALTIME SOCKET EMITTER
-# =========================
-def socket_sender():
-
-    while True:
-
-        try:
-
-            socketio.emit(
-                "update",
-                web_data
-            )
-
-            socketio.sleep(2)
-
-        except Exception as e:
-
-            print(
-                f"SOCKET ERROR: {e}"
-            )
-
-            socketio.sleep(2)
-
-# =========================
-# START SERVER
+# START
 # =========================
 if __name__ == "__main__":
 
-    print(
-        "===================================="
-    )
-
-    print(
-        " BINANCE AI SCREENER STARTED "
-    )
-
-    print(
-        "===================================="
-    )
-
-    # =========================
-    # START SOCKET EMITTER
-    # =========================
     socketio.start_background_task(
-        socket_sender
+        start_bot
     )
 
-    # =========================
-    # START BOT ENGINE
-    # =========================
-    socketio.start_background_task(
-        run_bot
-    )
-
-    # =========================
-    # RUN FLASK
-    # =========================
     socketio.run(
 
         app,
