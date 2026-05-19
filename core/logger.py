f4471b05d2238949b1847b5e950af58ec56f21a8
Anseltsmm <anseltsm24@gmail.com>
2026-05-19 18:27:46 +0700

    Update logger.py

--- core/logger.py
+++ core/logger.py
@@ -4,14 +4,58 @@ import os
 DATASET_FILE = "data/ai_dataset.csv"
 
 HEADERS = [
+
+    # =========================
+    # TREND
+    # =========================
+
+    "trend",
+    "structure",
+
+    # =========================
+    # INDICATORS
+    # =========================
+
     "rsi",
     "adx",
+
+    "ema_fast",
+    "ema_slow",
+
+    "macd",
+    "macd_signal",
+
+    # =========================
+    # SCORES
+    # =========================
+
     "long_score",
     "short_score",
+
+    # =========================
+    # MTF
+    # =========================
+
     "bullish_tf",
     "bearish_tf",
+
+    # =========================
+    # SNR
+    # =========================
+
     "support_distance",
     "resistance_distance",
+
+    # =========================
+    # VOLUME
+    # =========================
+
+    "high_volume",
+
+    # =========================
+    # AI TARGET
+    # =========================
+
     "signal",
     "result"
 ]
@@ -21,6 +65,8 @@ HEADERS = [
 # =========================================
 
 def init_dataset():
+
+    os.makedirs("data", exist_ok=True)
 
     if not os.path.exists(DATASET_FILE):
 
